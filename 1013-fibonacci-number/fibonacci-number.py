e0b401ebf663b46b16f5773c0d621594f0965d50
shabreenkhan <shabreenkhan567@gmail.com>
2026-08-11 10:22:48 +0530

    Time: 53 ms (42.77%) | Memory: 19.1 MB (88.59%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -0,0 +1,11 @@
+class Solution:
+    def fib(self, n: int) -> int:
+        if n==0:
+            return 0
+        a=0
+        b=1
+        for i in range(n):
+            c=a+b
+            a=b
+            b=c
+        return a
